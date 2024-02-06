bf47210bcc22cdcb2f5ebac54da53fdd5e75778b
Mnatsakanyan <104078312+armeanco@users.noreply.github.com>
2024-02-06 21:13:26 +0300

    Update sol.py

--- Distance And Similarities/sol.py
+++ Distance And Similarities/sol.py
@@ -1,27 +1,21 @@
 class Metrics():
    def euclidean_distance(self, X, Y):
        euclidean = 0
-       mx = len(X) if len(X) >= len(Y) else len(Y) 
-       for x in range(0, mx):
+       for x in range(0, len(X) if len(X) >= len(Y) else len(Y)):
            if x < len(Y) and x < len(X):
                euclidean += (X[x] - Y[x])**2
        return euclidean**(1/2)
 
    def manhattan_distance(self, X, Y):
        manhattan = 0
-       mx = len(X) if len(X) >= len(Y) else len(Y) 
-       for x in range(0, mx):
+       for x in range(0, len(X) if len(X) >= len(Y) else len(Y)):
            if x < len(Y) and x < len(X):
                manhattan += abs(X[x] - Y[x])
        return manhattan
 
    def cosine_similarity(self, X, Y):
-       cosine = 0
-       mx = len(X) if len(X) >= len(Y) else len(Y)
-       sum = 0
-       sqrt_term_a = 0
-       sqrt_term_b = 0
-       for x in range(0, mx):
+       sum = sqrt_term_a = sqrt_term_b = 0
+       for x in range(0, len(X) if len(X) >= len(Y) else len(Y)):
            if x < len(Y):
                sqrt_term_b += Y[x]**2
            if x < len(X):
@@ -32,29 +26,51 @@ class Metrics():
        return cosine
      
    def jaccard_similarity(self, X, Y):
-       jaccard = 0
-       plus = 0
-       minus = 0
-       union = 0
-       find_X = [0] * 2000
-       find_Y = [0] * 2000
-       for x in range(0, len(X)):
-           if X[x] < 0:
-               find_X[X[x]*-1000] += 1
-           else:
-               find_X[X[x]] += 1
-       for x in range(0, len(Y)):
-           if Y[x] < 0:
-               find_Y[Y[x]*-1000] += 1
-           else:
-               find_Y[Y[x]] += 1
-       for x in range(0, len(find_X)):
-           if find_X[x] >= 1 and find_Y[x] >= 1:
-               plus += find_X[x] if find_X[x] >= find_Y[x] else find_Y[x]
-               union += 1
-       jaccard = union/(len(X)+len(Y)-plus)
-       return jaccard if union > 1 else union
-     
+       union = count = 0
+       hash = [0] * 2000
+       find = [0] * 2000
+       for x in range(0, len(X) if len(X) >= len(Y) else len(Y)):
+           if x < len(X):
+               if X[x] < 0:
+                   if find[X[x]*-1000] < 2:
+                       find[X[x]*-1000] += 1
+                   if hash[X[x]*-1000] == 0:
+                       hash[X[x]*-1000] += 1
+                       count += 1
+                   if find[X[x]*-1000] > 1:
+                       union += 1
+                       find[X[x]*-1000] = 0
+               if X[x] >= 0:
+                    if find[X[x]] < 2:
+                        find[X[x]] += 1
+                    if hash[X[x]] == 0:
+                        hash[X[x]] += 1
+                        count += 1
+                    if find[X[x]] > 1:
+                        union += 1
+                        find[X[x]] = 0
+           if x < len(Y):
+                if Y[x] < 0:
+                    if find[Y[x]*-1000] < 2:
+                        find[Y[x]*-1000] += 1
+                    if hash[Y[x]*-1000] == 0:
+                        hash[Y[x]*-1000] += 1
+                        count += 1
+                    if find[Y[x]*-1000] > 1:
+                        union += 1
+                        find[Y[x]*-1000] = 0
+                if Y[x] >= 0:
+                    if find[Y[x]] < 2:
+                        find[Y[x]] += 1
+                    if hash[Y[x]] == 0:
+                        hash[Y[x]] += 1
+                        count += 1
+                    if find[Y[x]] > 1:
+                        union += 1
+                        find[Y[x]] = 0
+       jaccard = union/count
+       return jaccard if count > 1 else count
+      
 def distances_and_similarities(X, Y):
    metrics = Metrics()
    return [metrics.euclidean_distance(X, Y),
